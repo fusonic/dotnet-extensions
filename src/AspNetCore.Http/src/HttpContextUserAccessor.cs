8dd3f25b751db854d44bbdc90d0f277013e29d0c
David Roth <david.roth@fusonic.net>
2019-06-19 07:19:40 +0000

    Added IUserAccessor abstraction + HttpContextUserAccesor adapter.

---
+++ src/AspNetCore.Http/src/HttpContextUserAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Fusonic.Extensions.Abstractions;
+using Microsoft.AspNetCore.Http;
+
+namespace Fusonic.Extensions.AspNetCore.Http
+{
+    public class HttpContextUserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public HttpContextUserAccessor(IHttpContextAccessor contextAccessor)
+            => this.contextAccessor = contextAccessor;
+
+        public ClaimsPrincipal User => contextAccessor.HttpContext.User;
+    }
+}
