816e7dfa59a5ace38c2cc9109070c571682cd571
David Roth <david.roth@fusonic.net>
2019-08-28 08:47:37 +0200

    remove comments from assemblyinfo

--- src/Validation/src/AssemblyInfo.cs
+++ src/Validation/src/AssemblyInfo.cs
@@ -1,12 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c3592a19-9896-4106-995a-fd306ab03abf")]
-
 [assembly: InternalsVisibleTo("Fusonic.Extensions.Validation.Tests")]
