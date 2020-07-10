700cf1213ba130c1d01dba38cc0f5876780752e7
Johannes Hartmann <johannes.hartmann@fusonic.net>
2020-07-10 09:59:38 +0200

    Added missing ViewLocalizer-Registration

--- src/Email/src/ContainerExtensions.cs
+++ src/Email/src/ContainerExtensions.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc.Localization;
 using SimpleInjector;
 
 namespace Fusonic.Extensions.Email
 {
     public static class ContainerExtensions
     {
-        /// <summary> Registers the components for sending emails. </summary>
+        /// <summary>
+        /// Registers the components for sending emails.
+        ///
+        /// Note: You need to register ViewLocalization in your services.
+        /// Example:
+        /// <code>
+        /// services.AddLocalization(options => options.ResourcesPath = "Resources/Localization");
+        /// 
+        /// services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)))
+        ///         .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+        /// </code>
+        /// </summary>
         public static void RegisterEmail(this Container container, Action<EmailOptions> configure)
         {
             var options = new EmailOptions();
@@ -16,6 +28,7 @@ namespace Fusonic.Extensions.Email
             container.RegisterInstance(new CssInliner(options));
             container.Register<IEmailRenderingService, RazorEmailRenderingService>();
             container.Register<ISmtpClient, SmtpClient>();
+            container.RegisterInstance<Func<IViewLocalizer>>(container.GetInstance<IViewLocalizer>);
         }
     }
 }
