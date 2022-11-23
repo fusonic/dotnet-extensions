1994573bc3925c2ee7cfac3da76a685b368cb533
Johannes Hartmann <johannes.hartmann@fusonic.net>
2022-11-23 18:18:13 +0100

    Added EnforceCodeStyleInBuild, added missing file headers

--- src/AspNetCore/test/Validation/RequestValidationDecoratorTests.cs
+++ src/AspNetCore/test/Validation/RequestValidationDecoratorTests.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Fusonic GmbH. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
 using FluentAssertions;
 using Fusonic.Extensions.AspNetCore.Validation;
 using MediatR;
--- src/UnitTests.EntityFrameworkCore.Npgsql/src/SimpleInjectorExtensions.cs
+++ src/UnitTests.EntityFrameworkCore.Npgsql/src/SimpleInjectorExtensions.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Fusonic GmbH. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for license information.
 
-using Fusonic.Extensions.UnitTests.SimpleInjector;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using SimpleInjector;
