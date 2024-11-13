36eca8f4de265e95b0b1556b2464b9b591cd5014
Paul Hagspiel <paul.hagspiel@fusonic.net>
2024-11-13 11:04:24 +0000

    feature(#86): Add an ef core no action delete convention

--- docs/EntityFrameworkCore/README.md
+++ docs/EntityFrameworkCore/README.md
@@ -1,3 +1,29 @@
 # EntityFrameworkCore
 
+- [EntityFrameworkCore](#entityframeworkcore)
+  - [Extensions](#extensions)
+  - [Conventions](#conventions)
+
+## Extensions
 Provides a set of extension methods to make common querying tasks easier. See `QueryableExtensions.cs`
+
+## Conventions
+Provides a more sensible delete convention than EF Core's [`CascadeDeleteConvention`](https://github.com/dotnet/efcore/blob/main/src/EFCore/Metadata/Conventions/CascadeDeleteConvention.cs). `NoActionDeleteConvention` sets `NO ACTION` on your foreign key per default instead of `CASCADE`, which forces all dependent foreign keys to also be deleted by the user.
+
+With the default EF Core behavior, it is easy to create bugs that wouldn't be caught automatically by your testing. For example when referencing a `User` in a `History` and deleting the `User`, the `History` would be deleted as well. This is most likely not the desired behavior. 
+Therefore, if such a behavior is intended it should either be implemented
+- by manually deleting all references in code
+- by explicitly overriding the delete behavior on your foreign key
+- by setting the foreign key as nullable
+
+See https://github.com/dotnet/efcore/issues/27479, https://learn.microsoft.com/en-us/ef/core/saving/cascade-delete and https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-FK for more information.
+
+To use this, add this to your `DbContext`:
+
+```cs
+protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+{
+     configurationBuilder.Conventions.Remove<CascadeDeleteConvention>();
+     configurationBuilder.Conventions.Add(static _ => new NoActionDeleteConvention());
+}
+```
---
+++ src/EntityFrameworkCore/src/NoActionDeleteConvention.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Fusonic GmbH. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Fusonic.Extensions.EntityFrameworkCore;
+
+/// <summary>
+///     A more reasonable delete convention than <see cref="CascadeDeleteConvention"/>, that sets the delete behavior to <see cref="DeleteBehavior.NoAction" /> for required foreign keys
+///     and <see cref="DeleteBehavior.ClientSetNull" /> for optional ones.
+/// </summary>
+/// <remarks>
+///     See <see href="https://aka.ms/efcore-docs-conventions">Model building conventions</see> for more information and examples.
+/// </remarks>
+public class NoActionDeleteConvention
+    : IForeignKeyAddedConvention,
+      IForeignKeyRequirednessChangedConvention
+{
+    public virtual void ProcessForeignKeyAdded(
+        IConventionForeignKeyBuilder relationshipBuilder,
+        IConventionContext<IConventionForeignKeyBuilder> context
+    )
+    {
+        var newRelationshipBuilder = relationshipBuilder.OnDelete(
+            GetTargetDeleteBehavior(relationshipBuilder.Metadata)
+        );
+
+        if (newRelationshipBuilder != null)
+        {
+            context.StopProcessingIfChanged(newRelationshipBuilder);
+        }
+    }
+
+    public virtual void ProcessForeignKeyRequirednessChanged(
+        IConventionForeignKeyBuilder relationshipBuilder,
+        IConventionContext<bool?> context
+    )
+    {
+        var newRelationshipBuilder = relationshipBuilder.OnDelete(
+            GetTargetDeleteBehavior(relationshipBuilder.Metadata)
+        );
+
+        if (newRelationshipBuilder != null)
+        {
+            context.StopProcessingIfChanged(newRelationshipBuilder.Metadata.IsRequired);
+        }
+    }
+
+    protected virtual DeleteBehavior GetTargetDeleteBehavior(IConventionForeignKey foreignKey)
+        => foreignKey.IsRequired ? DeleteBehavior.NoAction : DeleteBehavior.ClientSetNull;
+}
