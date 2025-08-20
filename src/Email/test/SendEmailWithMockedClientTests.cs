38a191779df503d67d0030eacb769326f8011643
Johannes Hartmann <johannes.hartmann@fusonic.net>
2025-08-20 13:19:07 +0200

    fix: SendEmail - fixed issue where standard MimeMessage-Headers were duplicated instead of overwritten when set in the request

---
+++ src/Email/test/SendEmailWithMockedClientTests.cs
@@ -0,0 +1,49 @@
+// Copyright (c) Fusonic GmbH. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
+using System.Globalization;
+using FluentAssertions;
+using Fusonic.Extensions.Email.Tests.Models;
+using MimeKit;
+using NSubstitute;
+using NSubstitute.ClearExtensions;
+using SimpleInjector;
+using Xunit;
+
+namespace Fusonic.Extensions.Email.Tests;
+
+public class SendEmailWithMockedClientTests : TestBase<SendEmailWithMockedClientTests.SendEmailFixture>
+{
+    private readonly ISmtpClient smtpClient;
+
+    public SendEmailWithMockedClientTests(SendEmailFixture fixture) : base(fixture)
+    {
+        smtpClient = GetInstance<ISmtpClient>();
+        smtpClient.ClearSubstitute();
+    }
+
+    [Fact]
+    public async Task SendEmail_StandardHeadersOverwritten()
+    {
+        await smtpClient.SendMailAsync(Arg.Do<MimeMessage>(ValidateEmail));
+
+        var model = new SendEmailTestEmailViewModel { SomeField = "Some field." };
+        await SendAsync(new SendEmail("recipient@fusonic.net", "The Recipient", new CultureInfo("de-AT"), model, Headers: new Dictionary<string, string> { ["Message-Id"] = "value" }));
+
+        await smtpClient.Received(1).SendMailAsync(Arg.Any<MimeMessage>());
+
+        static void ValidateEmail(MimeMessage email)
+        {
+            email.Headers.Should().ContainSingle(s => s.Field.Equals("Message-Id", StringComparison.OrdinalIgnoreCase));
+            email.Headers["Message-Id"].Should().Be("value");
+        }
+    }
+
+    public class SendEmailFixture : TestFixture
+    {
+        protected override void RegisterDependencies(Container container)
+        {
+            container.RegisterSingleton(() => Substitute.For<ISmtpClient>());
+        }
+    }
+}
